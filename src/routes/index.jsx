20c83a18eadf82fb24fb3547abe8148fc5fcb457
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-09 02:28:35 -0300

    feat: adicionando pagina de trilhas na rota

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login"
+import Trails from "../pages/Trails";
+
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/trilhas" element={<Trails />} />
     </Routes>
   );
 }
