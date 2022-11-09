ce342fd425877b96855ce14c905d2b6a2f2e6fcd
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-09 02:29:05 -0300

    feat: criando primeiros componentes

---
+++ src/shared/components/CardTrail/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+const CardTrail = () => {
+  return (
+    <div>
+      <Box
+        sx={{
+          width: 270,
+          height: 340,
+          border: "1px solid black",
+        }}
+      />
+      <strong>Desenvolvimento Full Stack</strong>
+      <p>Trilha</p>
+    </div>
+  );
+};
+
+export default CardTrail;
